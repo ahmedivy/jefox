6de54d73c2f3c4a90385d9733cbf88302eaf8d31
ahmed <ahmadabdullah4919@gmail.com>
2023-08-09 22:29:04 +0500

    add referrals api endpoint

--- app/(dash)/referrals/page.jsx
+++ app/(dash)/referrals/page.jsx
@@ -1,3 +1,31 @@
-export default function Page() {
-  return null;
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "@/lib/auth";
+
+async function getReferrals(username) {
+  const res = await fetch(
+    `${process.env.NEXTAUTH_URL}/api/users/${username}/referrals`
+  );
+  const data = await res.json();
+  return data;
 }
+
+async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  const referrals = await getReferrals(session.user.username);
+
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold pl-2">Referrals</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 w-full gap-4 pt-8"></div>
+    </main>
+  );
+}
+
+export default Page;
---
+++ app/api/users/[username]/referrals/route.js
@@ -0,0 +1,83 @@
+import prisma from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  const username = params.username;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+      username: true,
+      firstname: true,
+      lastname: true,
+      leftReferralsIds: true,
+      rightReferralsIds: true,
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json({
+      success: false,
+      error: "User not found",
+    });
+  }
+
+  const fetchLefts = async (user) => {
+    const lefts = await prisma.user.findMany({
+      where: {
+        id: {
+          in: user.leftReferralsIds,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: {
+        id: true,
+        username: true,
+        firstname: true,
+        lastname: true,
+        createdAt: true,
+        image: true,
+      },
+    });
+    return lefts;
+  };
+
+  const fetchRights = async (user) => {
+    const rights = await prisma.user.findMany({
+      where: {
+        id: {
+          in: user.rightReferralsIds,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: {
+        id: true,
+        username: true,
+        firstname: true,
+        lastname: true,
+        createdAt: true,
+        image: true,
+      },
+    });
+    return rights;
+  };
+
+  const [lefts, rights] = await Promise.all([
+    fetchLefts(user),
+    fetchRights(user),
+  ]);
+
+  return NextResponse.json({
+    success: true,
+    user,
+    lefts,
+    rights,
+  });
+}
