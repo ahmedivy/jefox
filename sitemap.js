cdbd00273e192d9d00e31e23a1b010c898e67094
ahmed <ahmadabdullah4919@gmail.com>
2023-08-22 19:28:29 +0500

    add sitemap

---
+++ sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://jefox.website",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://jefox.website/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://jefox.website/dashboard",
+      lastModified: new Date(),
+    },
+  ];
+}
