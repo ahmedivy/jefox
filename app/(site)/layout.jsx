0c51451abd9bc07970d2c0d94a3976934ada3f92
ahmed <ahmadabdullah4919@gmail.com>
2023-08-22 17:04:39 +0500

    add hero

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -32,9 +32,9 @@ export default function RootLayout({ children }) {
           <AuthProvider>
             <Header />
             {children}
-            <Toaster />
           </AuthProvider>
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
